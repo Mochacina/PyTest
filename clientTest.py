b9338ae363814daee0f322b064df0862b0599ed6
Mochacina <nayuta5554@gmail.com>
2023-11-28 11:02:02 +0900

    데이터 수신 추가

--- clientTest.py
+++ clientTest.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, scrolledtext
 import socket
 import threading
 
@@ -38,6 +38,7 @@ class ClientApp:
         self.port = 0
         self.client_socket = None
         self.connected = False
+        self.new_window = None
 
     def toggle_connection(self):
         if self.connected:
@@ -55,6 +56,15 @@ class ClientApp:
             self.connected = True
             self.connect_button.config(text="연결 해제")
             self.send_button.config(state=tk.NORMAL)
+            
+            # 새로운 창 생성
+            self.create_new_window()
+            
+            # 새로운 창에 연결된 소켓을 전달
+            # self.new_window.client_socket = self.client_socket
+
+            # 서버로부터 수신한 데이터를 처리할 쓰레드 시작
+            threading.Thread(target=self.receive_data).start()
         except Exception as e:
             print(f"연결 오류: {e}")
 
@@ -64,6 +74,7 @@ class ClientApp:
         self.connected = False
         self.connect_button.config(text="연결")
         self.send_button.config(state=tk.DISABLED)
+        self.close_new_window()
 
     def send_data(self):
         data = self.input_text_entry.get()
@@ -71,6 +82,47 @@ class ClientApp:
             self.client_socket.send(data.encode())
         except Exception as e:
             print(f"데이터 전송 오류: {e}")
+            
+    def receive_data(self):
+        while self.connected:
+            try:
+                data = self.client_socket.recv(1024).decode()
+                if not data:
+                    break
+                # 서버로부터 수신한 데이터를 새로운 창에 추가
+                if self.new_window:
+                    self.add_received_data(f"받은 데이터: {data}")
+            except Exception as e:
+                print(f"데이터 수신 오류: {e}")
+                break
+            
+    def create_new_window(self):
+        # 수신된 데이터를 표시할 새로운 창 생성
+        self.new_window = tk.Toplevel(self.root)
+        self.new_window.title("수신된 데이터")
+
+        # scrolledtext 위젯 생성
+        self.received_data_text = scrolledtext.ScrolledText(self.new_window, wrap=tk.WORD, width=40, height=10)
+        self.received_data_text.pack(padx=10, pady=10)
+
+        # 새로운 창이 닫힐 때 연결 해제
+        self.new_window.protocol("WM_DELETE_WINDOW", self.on_new_window_close)
+
+    def close_new_window(self):
+        # 새로운 창이 닫힐 때 호출되는 함수
+        if self.new_window:
+            self.new_window.destroy()
+            self.new_window = None
+
+    def on_new_window_close(self):
+        # 새로운 창이 닫힐 때 연결 해제
+        self.disconnect()
+        
+    def add_received_data(self, data):
+        # 수신된 데이터를 텍스트 위젯에 추가
+        self.received_data_text.insert(tk.END, data + "\n")
+        # 스크롤을 항상 가장 아래로 이동
+        self.received_data_text.yview(tk.END)
 
 if __name__ == "__main__":
     root = tk.Tk()
